6e485e09773068773990b892f3a3094ff1337a88
inkthorne <inkthorne@gmail.com>
2025-06-01 12:02:29 -0600

    removed unused files 'option.rs', 'message.rs', & 'response.rs'

--- src/ollama/mod.rs
+++ src/ollama/mod.rs
@@ -1,14 +1,5 @@
-pub mod message;
-pub use message::*;
-
 pub mod ollama;
 pub use ollama::*;
-
-pub mod option;
-pub use option::*;
-
-pub mod response;
-pub use response::*;
 
 pub mod tool;
 pub use tool::*;
